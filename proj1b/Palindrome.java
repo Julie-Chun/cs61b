08cd31ecc1b919ff3ec9b82007d6e375962d0661
Julie Chun <j.chun@berkeley.edu>
2020-09-18 04:53:08 -0700

    finished and tested Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -4,11 +4,38 @@
  */
 
 public class Palindrome {
-
-    /** returns a deque with each letter of the String in order. */
-    public Deque<Character> wordToDeque(String word){
-        return null;
+    /** returns a deque with each letter of the String in order as char. */
+    public Deque<Character> wordToDeque(String word) {
+        LinkedListDeque<Character> link = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i); //charAt learned from beginnerbook.com
+            link.addLast(c);
+        }
+        return link;
     }
 
+    /** return true if word is a palindrome. */
+    public boolean isPalindrome(String word) {
+        Deque<Character> deq = wordToDeque(word);
+        int len = word.length();
+
+        if (word != null) {
+            if (len <= 1 && len >= 0) {
+                return true;
+            } else {
+                char first = deq.removeFirst();
+                char last = deq.removeLast();
+                if (last != first) {
+                    return false;
+                }
+                return isPalindrome(word.substring(1,word.length() - 1)); //learned substring method from beginnerbook.com
+            }
+        }
+        return false;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc){
+        return false;
+    }
 
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -14,5 +14,60 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
+
+        Deque de = palindrome.wordToDeque("");
+        String a = "";
+        for (int i = 0; i < "".length(); i++) {
+            a += de.removeFirst();
+        }
+        assertEquals("", a);
+
+        Deque deq = palindrome.wordToDeque("hello there");
+        String ac = "";
+        for (int i = 0; i < "hello there".length(); i++) {
+            ac += deq.removeFirst();
+        }
+        assertEquals("hello there", ac);
+
+        Deque dq = palindrome.wordToDeque("hm...");
+        String act = "";
+        for (int i = 0; i < "hm...".length(); i++) {
+            act += dq.removeFirst();
+        }
+        assertEquals("hm...", act);
+
+        Deque d1 = palindrome.wordToDeque("racecar");
+        String a1 = "";
+        for (int i = 0; i < "racecar".length(); i++) {
+            a1 += d1.removeFirst();
+        }
+        assertEquals("racecar", a1);
+
+        Deque d2 = palindrome.wordToDeque("noon");
+        String a2 = "";
+        for (int i = 0; i < "noon".length(); i++) {
+            a2 += d2.removeFirst();
+        }
+        assertEquals("noon", a2);
+
+        Deque d3 = palindrome.wordToDeque("aba");
+        String a3 = "";
+        for (int i = 0; i < "aba".length(); i++) {
+            a3 += d3.removeFirst();
+        }
+        assertEquals("aba", a3);
+    }
+
+    @Test
+    public void testisPalindrome() {
+        assertTrue(palindrome.isPalindrome(""));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome("racecar"));
+        assertTrue(palindrome.isPalindrome("noon"));
+        assertFalse(palindrome.isPalindrome("horse"));
+        assertTrue(palindrome.isPalindrome("aba"));
+        assertTrue(palindrome.isPalindrome("abba"));
+        assertFalse(palindrome.isPalindrome("aaaaab"));
+        assertFalse(palindrome.isPalindrome("rancor"));
     }
 }
