63d36d6af563f21f01c07a199d2b5bcbc15d2f8d
Julie Chun <j.chun@berkeley.edu>
2020-09-26 00:00:17 -0700

    changed addLast to multiply 3 instead of 2

--- lab5/AList.java
+++ lab5/AList.java
@@ -32,7 +32,7 @@ public class AList<Item> {
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
         if (size >= items.length) {
-            resize((int) (size * 2.00));
+            resize((int) (size * 3));
         }
 
         items[size] = x;
