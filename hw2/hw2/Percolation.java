f058f26c484314802393ac1884b1b91fd86f9f5f
Julie Chun <j.chun@berkeley.edu>
2020-10-05 23:01:22 -0700

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,129 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /* the grid. */
+    int[][] grid;
 
+    /* size of square grid. */
+    private int size;
+
+    /* number of open sites. */
+    private int numOpen;
+
+    /* keeps records of openess. */
+    boolean[][] openSpots;
+
+    /* virtual sites.*/
+    private WeightedQuickUnionUF tree;
+
+    /* creates a N-by-N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        size = N;
+        grid = new int[N][N];
+        openSpots = new boolean[N][N];
+        tree = new WeightedQuickUnionUF(size + 999);
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                grid[r][c] = c + (r * size);
+                openSpots[r][c] = false;
+                if (r == 0) {
+                    tree.union(size + 990, grid[r][c]);
+                }
+                /*
+                if (r == size - 1) {
+                    tree.union(grid[r][c], size + 998);
+                }
+
+                 */
+            }
+        }
+    }
+
+    /* opens the site (row, col) if it is not open already. */
+    public void open(int row, int col) {
+        if (row < 0 || row > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else if (col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else {
+            numOpen++;
+            openSpots[row][col] = true;
+
+            if (row - 1  >= 0 && openSpots[row - 1][col]) {
+                tree.union(grid[row][col], grid[row - 1][col]);
+            }
+            if (row + 1 < size && openSpots[row + 1][col]) {
+                tree.union(grid[row + 1][col], grid[row][col]);
+            }
+            if (col - 1  >= 0 && openSpots[row][col - 1]) {
+                tree.union(grid[row][col], grid[row][col - 1]);
+            }
+            if (col + 1 < size && openSpots[row][col +  1]) {
+                tree.union(grid[row][col + 1], grid[row][col]);
+            }
+        }
+    }
+
+    /* checks if the site (row, col) is open. */
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException("row is out of bounds.");
+        }
+        if (col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException("column is out of bounds.");
+        }
+
+        return openSpots[row][col];
+    }
+
+    /* checks if the site (row, col) is full. */
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException("row is out of bounds.");
+        }
+        if (col < 0 || col > size - 1) {
+            throw new java.lang.IndexOutOfBoundsException("column is out of bounds.");
+        }
+
+        return tree.connected(size + 990, grid[row][col]) && isOpen(row, col);
+    }
+
+    /* returns the number of open sites. */
+    public int numberOfOpenSites() {
+        return numOpen;
+    }
+
+    /* checks if the system percolates. */
+    public boolean percolates() {
+        boolean connect = false;
+        for (int i = 0; i < size; i++) {
+            int lastRow = i + ((size - 1) * size);
+            connect = connect || tree.connected(size + 990, lastRow);
+        }
+        return connect;
+    }
+
+    /* For unit testing (not required, but keep this here for the autograder). */
+    public static void main(String[] args) {
+        Percolation per = new Percolation(4);
+        int num0 = per.numberOfOpenSites();
+        boolean a = per.isFull(0, 0); // false
+        per.open(0, 0);
+        int num1 = per.numberOfOpenSites(); // 1
+        per.open(1, 1);
+        int num2 = per.numberOfOpenSites(); // 2
+        boolean b = per.isOpen(1, 1); // true
+        boolean c = per.isFull(1, 1); // false
+        boolean d = per.isFull(0, 0); //true
+        per.open(3, 0);
+        int num3 = per.numberOfOpenSites(); // 3
+        boolean e = per.percolates(); //false
+        per.open(1, 0);
+        boolean f = per.isFull(1, 1); // true
+        per.open(2, 0);
+        int num5 = per.numberOfOpenSites(); // 5
+        boolean g = per.isFull(2, 0); // true
+        boolean h = per.percolates(); // true
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,44 @@
 package hw2;
 
 public class PercolationStats {
+    /* fraction of open sites per experiment.*/
+    private double[] frac;
 
+    /* Perform T independent experiments on an N-by-N grid. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        Percolation p = pf.make(N);
+        frac = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            while (!p.percolates()) {
+                int r = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                int c = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                p.open(r, c);
+            }
+            frac[i] = ((double) p.numberOfOpenSites()) / (double) (N * N);
+        }
+    }
+
+    /* Sample mean of percolation threshold. */
+    public double mean() {
+        return edu.princeton.cs.introcs.StdStats.mean(frac);
+    }
+
+    /* Sample standard deviation of percolation threshold. */
+    public double stddev() {
+        return edu.princeton.cs.introcs.StdStats.stddev(frac);
+    }
+
+    /* Low endpoint of 95% confidence interval. */
+    public double confidenceLow() {
+        return (mean() - ((1.96 * stddev())) / Math.sqrt(frac.length));
+    }
+
+    /* High endpoint of 95% confidence interval. */
+    public double confidenceHigh() {
+        return (mean() + ((1.96 * stddev())) / Math.sqrt(frac.length));
+    }
 }
