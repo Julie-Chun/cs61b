fe4c1ab6f29961f8489a86777055ef2db5aeca80
Julie Chun <j.chun@berkeley.edu>
2020-11-29 10:50:25 -0800

    fixing resizeUp and resizeDown

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -31,85 +31,124 @@ public class ArrayDeque<T> {
     public void addFirst(T item) {
         items[nextFirst] = item;
         size++;
+        nextFirst--;
 
-        if (nextFirst == 0) {
+        if (nextFirst < 0) {
             nextFirst = items.length - 1;
-        } else {
-            nextFirst--;
         }
 
-        if (size >= items.length) {
+        if (size > items.length - 1) {
             resizeUp();
         }
-    }
 
+    }
     /** adds @param item to the end of the list. */
     public void addLast(T item) {
         items[nextLast] = item;
         size++;
+        nextLast++;
+
         if (nextLast == items.length) {
             nextLast = 0;
-        } else {
-            nextLast++;
         }
 
-        if (size >= items.length) {
+        if (size > items.length - 1) {
             resizeUp();
         }
     }
 
     /** rescales the list so that its usage factor is more
      * than 25% and the nextFront and nextLast has a space in the list. */
+    private void resize() {
+        if (nextFirst < 0) {
+            T[] larger = (T[]) new Object[items.length + 1];
+            System.arraycopy(items, 0, larger, nextFirst + 2, items.length - 1);
+            items = larger;
+            nextFirst++;
+            nextLast++;
+        }
+
+        if (nextLast >= items.length) {
+            T[] larger = (T[]) new Object[items.length + 1];
+            System.arraycopy(items, 0, larger, 0, items.length);
+            items = larger;
+        }
+
+        if (items.length > 15) {
+            double usageFactor = (double) size / (double) items.length;
+            if (usageFactor < 0.25) {
+                T[] smaller = (T[]) new Object[size + 2];
+                System.arraycopy(items, nextFirst, smaller, 0, size + 2);
+                items = smaller;
+                nextLast = size + 1;
+                nextFirst = 0;
+            }
+        }
+
+    }
+
+    /** rescales the deque to twice its size once it is full. */
     private void resizeUp() {
-        int first = nextFirst + 1;
+        int first = nextFirst - 1;
+        int last = nextLast + 1;
 
-        if (nextFirst >= items.length) {
+        if (nextLast == 0) {
+            last = items.length - 1;
+        }
+
+        if (nextFirst == items.length - 1) {
             first = 0;
         }
 
-        T[] holder = (T[]) new Object[size * 2];
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null) {
-                holder[i] = items[i];
-            }
+        if (first < last) {
+            T[] holder = slice(items, first, last);
+            T[] copy = (T[]) new Object[items.length * 2];
+            System.arraycopy(holder, 0, copy, first, holder.length);
+            items = holder;
+        } else if (first > last) {
+            T[] front = slice(items, 0, last);
+            T[] back = slice(items, first, items.length - 1);
+            T[] copy = (T[]) new Object[items.length * 2];
+            System.arraycopy(front, 0, copy, 0, front.length);
+            System.arraycopy(back, 0, copy, first, back.length);
+            items = merge(front, back);
         }
-        //System.arraycopy(items, first, holder, 0, items.length - first);
 
-        items = holder;
+        System.out.println("resize UP");
     }
 
-    /** rescales the list so that its usage factor is more
-     * than 25% and the nextFront and nextLast has a space in the list. */
+    /** rescales the list so that its usage factor is more than 25%. */
     private void resizeDown() {
-        int first = nextFirst + 1;
+        int first = nextFirst - 1;
+        int last = nextLast + 1;
 
-        if (nextFirst >= items.length) {
+        if (nextLast == 0) {
+            last = items.length - 1;
+        }
+
+        if (nextFirst == items.length - 1) {
             first = 0;
         }
 
-        T[] holder = (T[]) new Object[items.length / 2];
-        int i = 0;
-        for (T it : items) {
-            if (it != null) {
-                holder[i] = it;
-                i += 1;
-            }
+        if (first < last) {
+            T[] holder = slice(items, first, last);
+            T[] copy = (T[]) new Object[items.length / 2];
+            System.arraycopy(holder, 0, copy, first, holder.length);
+            items = holder;
+        } else if (first > last) {
+            T[] front = slice(items, 0, last);
+            T[] back = slice(items, first, items.length - 1);
+            T[] copy = (T[]) new Object[items.length / 2];
+            System.arraycopy(front, 0, copy, 0, front.length);
+            System.arraycopy(back, 0, copy, first, back.length);
+            items = merge(front, back);
         }
-/*
-        if (nextLast != 0) {
-            System.arraycopy(items, 0, holder, items.length - first, nextLast);
-        }
-        
- */
 
-        items = holder;
-        nextFirst = items.length - 1;
-        nextLast = size;
+        System.out.println("resize Down");
     }
-
     /** @return true if the array list is empty. */
     public boolean isEmpty() {
-        for (int i = 0; i < items.length; i++) {
+        for (int i = nextFirst + 1; i < size + nextFirst + 1; i++) {
             if (items[i] != null) {
                 return false;
             }
@@ -124,24 +163,10 @@ public class ArrayDeque<T> {
 
     /** prints the list of size size. */
     public void printDeque() {
-        int first = nextFirst + 1;
-
-        if (nextFirst >= items.length) {
-            first = 0;
-        }
-
         if (size > 0) {
-            for (int i = first; i < items.length; i++) {
+            for (int i = 0; i < items.length - 1; i++) {
                 if (items[i] != null) {
                     System.out.print(items[i] + " ");
-                }
-            }
-
-            if (nextLast != 0) {
-                for (int i = 0; i < first; i++) {
-                    if (items[i] != null) {
-                        System.out.print(items[i] + " ");
-                    }
                 }
             }
         }
@@ -151,21 +176,9 @@ public class ArrayDeque<T> {
     /** @return the first item of the list and removes it from the list. */
     public T removeFirst() {
         if (size > 0) {
-            int first = nextFirst + 1;
-
-            if (nextFirst == items.length - 1) {
-                first = 0;
-            }
-
-            T pop = items[first];
-            items[first] = null;
-
-            if (nextFirst == items.length) {
-                nextFirst = 0;
-            } else {
-                nextFirst++;
-            }
-
+            T pop = items[nextFirst + 1];
+            items[nextFirst + 1] = null;
+            nextFirst++;
             size--;
 
             if (items.length > 15) {
@@ -183,21 +196,9 @@ public class ArrayDeque<T> {
     /** @return the last item of the list and removes it from the list. */
     public T removeLast() {
         if (size > 0) {
-            int last = nextLast - 1;
-
-            if (nextLast == 0) {
-                last = items.length - 1;
-            }
-
-            T pop = items[last];
-            items[last] = null;
-
-            if (nextLast == 0) {
-                nextLast = items.length;
-            } else {
-                nextLast--;
-            }
-
+            T pop = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast--;
             size--;
 
             if (items.length > 15) {
@@ -215,9 +216,52 @@ public class ArrayDeque<T> {
     /** @return the item of the list at @param index . */
     public T get(int index) {
         if (index < size) {
-            return items[(nextFirst + 1 + index) % items.length];
+            return items[nextFirst + 1 + index];
         }
         return null;
     }
 
+    public static void main(String[] args) {
+        ArrayDeque<Integer> array = new ArrayDeque<>();
+
+        array.addFirst(1);
+        array.addFirst(2);
+        array.addFirst(3);
+        array.addFirst(4);
+        array.addFirst(5);
+        array.addLast(6);
+        array.addLast(7);
+        array.addLast(8);
+
+        array.printDeque();
+        System.out.println(array.nextFirst);
+        System.out.println(array.nextLast);
+    }
+
+    /** returns a smaller array from index start to index end inclusive. */
+    private T[] slice(T[] array, int start, int end) {
+        T[] sliced = (T[]) new Object[end - start + 1];
+        int index = 0;
+        for (int i = start; i <= end; i++) {
+            sliced[index] = array[i];
+        }
+        return sliced;
+    }
+
+    /** returns a single array of arr1 and arr2 merged together respectively and in order. */
+    private T[] merge(T[] arr1, T[] arr2) {
+        T[] sliced = (T[]) new Object[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            sliced[i] = arr1[i];
+        }
+
+        int count = 0;
+        for (int j = arr1.length; j < sliced.length; j++) {
+            sliced[j] = arr2[count];
+            count++;
+        }
+
+        return sliced;
+    }
 }
